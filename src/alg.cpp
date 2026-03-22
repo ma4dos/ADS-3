d6338586cf6e22b86d6d122334ea0292931f0790
ma4dos <dosuzhaya.mari@bk.ru>
2026-03-22 19:27:05 +0300

    Update alg.cpp
    Досужая Мария 25-ист-4-2

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,17 +1,42 @@
-// Copyright 2025 NNTU-CS
 #include <cstdint>
+#include <unordered_map>
+#include <algorithm>
 #include "alg.h"
 
 uint64_t collatzMaxValue(uint64_t num) {
-  return 1;
+    static std::unordered_map<uint64_t, uint64_t> cache;
+    if (num == 1) return 1;
+    auto it = cache.find(num);
+    if (it != cache.end()) return it->second;
+    uint64_t next = (num % 2 == 0) ? num / 2 : 3 * num + 1;
+    uint64_t maxVal = std::max(num, collatzMaxValue(next));
+    cache[num] = maxVal;
+    return maxVal;
 }
 
 unsigned int collatzLen(uint64_t num) {
-  return 1;
+    static std::unordered_map<uint64_t, unsigned int> cache;
+    if (num == 1) return 1;
+    auto it = cache.find(num);
+    if (it != cache.end()) return it->second;
+    uint64_t next = (num % 2 == 0) ? num / 2 : 3 * num + 1;
+    unsigned int len = 1 + collatzLen(next);
+    cache[num] = len;
+    return len;
 }
 
 unsigned int seqCollatz(unsigned int *maxlen,
                         uint64_t lbound,
                         uint64_t rbound) {
-  return 1;
+    unsigned int bestLen = 0;
+    uint64_t bestNum = 0;
+    for (uint64_t i = lbound; i <= rbound; ++i) {
+        unsigned int len = collatzLen(i);
+        if (len > bestLen) {
+            bestLen = len;
+            bestNum = i;
+        }
+    }
+    *maxlen = bestLen;
+    return bestNum;
 }
